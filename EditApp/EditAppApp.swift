a4ce1aaa47c360d9e1bac4afe4a40cff88ddc26a
Ron Lane <theartfuldodger2012@gmail.com>
2024-04-20 09:35:14 -0400

    Initial Commit

---
+++ EditApp/EditAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EditAppApp.swift
+//  EditApp
+//
+//  Created by Ron Lane on 4/20/24.
+//
+
+import SwiftUI
+
+@main
+struct EditAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
